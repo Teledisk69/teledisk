d9b568bab8e7e1730ab56ada5946ea725984e584
dastagirS <shroud464@gmail.com>
2024-07-04 20:03:58 +0530

    4/7/24 Commit 4 fix error in install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo sudo apt-get update
-echo sudo apt-get upgrade
-echo sudo apt-get install make git zlib1g-dev libssl-dev gperf cmake g++
-echo git clone --recursive https://github.com/tdlib/telegram-bot-api.git
-echo cd telegram-bot-api
-echo rm -rf build
-echo mkdir build
-echo cd build
-echo cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=.. ..
-echo cmake --build . --target install
-echo cd ../..
-echo ls -l telegram-bot-api/bin/telegram-bot-api*
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install make git zlib1g-dev libssl-dev gperf cmake g++
+git clone --recursive https://github.com/tdlib/telegram-bot-api.git
+cd telegram-bot-api
+rm -rf build
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=.. ..
+cmake --build . --target install
+cd ../..
+ls -l telegram-bot-api/bin/telegram-bot-api*
